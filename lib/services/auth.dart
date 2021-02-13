6113a8c68cf00a25db453971603314bfa83b26f4
hammerandnail <yelionel26@gmail.com>
2021-02-14 06:53:04 +0800

    #7 - Streams

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -9,6 +9,11 @@ class AuthService {
     return user != null ? UserModel.User(uid: user.uid) : null;
   }
 
+  // auth change user stream
+  Stream<UserModel.User> get user {
+    return _auth.authStateChanges().map(_userFromFirebaseUser);
+  }
+
   // sign in anon
   Future signInAnon() async {
     try {
