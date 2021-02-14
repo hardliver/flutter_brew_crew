1a792219da681f49a1474fb5f9aa1acaa804a29e
hammerandnail <yelionel26@gmail.com>
2021-02-14 10:36:19 +0800

    #19 - Brew Model

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_brew_crew/models/brew.dart';
 import 'package:flutter_brew_crew/screens/home/brew_list.dart';
 import 'package:flutter_brew_crew/services/auth.dart';
 import 'package:flutter_brew_crew/services/database.dart';
@@ -10,7 +10,7 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<QuerySnapshot>.value(
+    return StreamProvider<List<Brew>>.value(
       value: DatabaseService().brews,
       child: Scaffold(
         backgroundColor: Colors.brown[50],
